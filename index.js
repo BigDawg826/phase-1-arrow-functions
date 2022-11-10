d5d1c2bc652b4d495253fa1049a066bc9b5d7a89
Darian Fultz <dfultz826@yahoo.com>
2022-11-10 16:59:10 -0500

    Lab completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function () {
+    return 2000/100;
+}
+
+const square = someNumber => {
+    return (someNumber * someNumber)
+}
+
+const add = (addend1, addend2) => {
+    return (addend1 + addend2)
+}
